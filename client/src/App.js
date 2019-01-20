9580e1c0b9a193bb48c5b64bd50109985bf1409c
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-01-20 15:49:12 -0500

    Create file with component for RandomDotsContainer and add link to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import './App.css'
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
 
 import TestComponent from './components_container/testComponent'
-import AboutTestComponent from './components_presentational/aboutTestComponent'
+// import AboutTestComponent from './components_presentational/aboutTestComponent'
+import RandomDotsContainer from './components_container/randomDotsContainer'
 
 class App extends Component {
 
@@ -15,12 +16,12 @@ class App extends Component {
 
           <ul>
             <li> <Link to="/"> Home </Link> </li>
-            <li> <Link to="/about"> About </Link> </li>
+            <li> <Link to="/randomdots"> Random Dots </Link> </li>
           </ul>
 
           <Switch>
             <Route path="/" exact component={TestComponent}/>
-            <Route path="/about" exact component={AboutTestComponent} />
+            <Route path="/about" exact component={RandomDotsContainer} />
           </Switch>
 
         </div>
---
+++ client/src/components_container/randomDotsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class RandomDotsContainer extends Component {
+  render(){
+    return(
+      <div class="container">
+        Random dots!
+      </div>
+    )
+  }
+}
+
+export default RandomDotsContainer
