1fbe0094f94f8fcf58a3db071f92d1b4d1bf29b9
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-01-20 17:03:03 -0500

    Make signif style updates for random dot...
    ... move font-size to body{}. This is the basis for all ems.
    
    Base dot container off ems, and have one random dot there, with
    size and position based of ems too.  Just playing around.

--- client/src/components_container/randomDotsContainer.js
+++ client/src/components_container/randomDotsContainer.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import '../randomDots.css'
 
 class RandomDotsContainer extends Component {
+
+
+
   render(){
     return(
       <div>
         <h1 class="text-centered">Random Dots!</h1>
         <div class="container">
+          <div class="dot" />
         </div>
       </div>
     )
