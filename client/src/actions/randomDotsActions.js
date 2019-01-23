da63d19218887b4285c885b06e68f573d8417475
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-01-23 16:03:51 -0500

    To randomDotActions.js, add functions for selectCoordinate

--- client/src/actions/randomDotsActions.js
+++ client/src/actions/randomDotsActions.js
@@ -28,13 +28,56 @@ export function addDot(payload) {
 class DotFactory {
 
   constructor() {
-    this.id = 0
+    this.idCounter = 0
   }
 
   createDot() {
-    this.id += 1
-    return new Dot(this.id)
+    this.idCounter += 1
+
+    let idNumber = this.idCounter
+    let color = this.selectColor()
+    let size = this.selectSize()
+    let sizeToString = `${size}em`
+    let xCoordinate = this.selectCoordinate(size)
+    let yCoordinate = this.selectCoordinate(size)
+
+    return new Dot(
+      { id: idNumber,
+        color: color,
+        height: size,
+        width: size,
+        top:
+
+
+      }
+    )
   }
+
+  selectColor() {
+    let colors = ["pink", "brown", "green", "blue", "grey", "red"]
+    let index = this.getRandom(0, 6)
+    return colors[index]
+  }
+
+  selectSize() {
+    return this.getRandom(1,3)
+  }
+
+  selectCoordinate(size) {
+    switch (size) {
+      case 1:
+        return getRandom(0,29)
+      case 2:
+        return getRandom(0,28)
+      case 3:
+        return getRandom(0,27)
+    }
+  }
+    // Get random number, inclusive of max and min
+  getRandom(min, max) {
+    return Math.floor(Math.random() * (max + 1) + min)
+  }
+
 }
 
 class Dot {
@@ -48,16 +91,6 @@ class Dot {
     // this.width: attributes.width
   }
 
-  selectColor() {
-    let colors = ["pink", "brown", "green", "blue", "grey", "red"]
-    let index = this.getRandom(0, 6)
-    return colors[index]
-  }
-
-    // Get random number, inclusive of max and min
-  getRandom(min, max) {
-    return Math.floor(Math.random() * (max + 1) + min)
-  }
 }
 
 //
