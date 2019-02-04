ebfb397fbe25c8b39f82c9b659dcf81010da2b99
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-02-04 14:52:45 -0500

    Remove bulky comments from randomDotsContainer

--- client/src/components_container/randomDotsContainer.js
+++ client/src/components_container/randomDotsContainer.js
@@ -8,34 +8,19 @@ class RandomDotsContainer extends Component {
 
   componentDidMount(){
 
-
     let dotFactory = createDotFactory()
     this.props.addDotFactory(dotFactory)
-    console.log("Wasting time 1")
-    console.log("wasting time 3")
     let firstDot = dotFactory.createDot()
     this.props.addDot(firstDot)
-    // debugger
-    // ---
-
-  // --- works:
-    // let firstDot = this.props.dotFactory.createDot()
-    // this.props.addDot(firstDot)
-
-    // this.props.createDotFactory()
-    // let firstDot = this.props.dotFactory.createDot()
-    // let dot = {id: 1, color: "green", height: "3em", width: "3em", left: "1em", bottom: "27em"}
-    // this.props.createDot(dot)
-
   }
 
   render(){
     return(
       <div>
-        Here's where the dots should be:
         <h1 class="text-centered">Random Dots!</h1>
         <div class="container">
           <div class="dot" />
+
         </div>
       </div>
     )
