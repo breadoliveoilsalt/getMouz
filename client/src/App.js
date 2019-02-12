39b782657fe00da0624370ab9e127a25d28b5dfe
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-02-12 15:09:28 -0500

    Add files for RainContainer

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
 import HomeComponent from './components_container/homeComponent'
 // import AboutTestComponent from './components_presentational/aboutTestComponent'
 import RandomDotsContainer from './components_container/randomDotsContainer'
+import RainContainer from './components_container/rainContainer'
 
 class App extends Component {
 
@@ -17,11 +18,13 @@ class App extends Component {
           <ul>
             <li> <Link to="/"> Home </Link> </li>
             <li> <Link to="/randomdots"> Random Dots </Link> </li>
+            <li> <Link to="/rain"> Make It Rain </Link> </li>
           </ul>
 
           <Switch>
-            <Route path="/" exact component={HomeComponent}/>
+            <Route path="/" exact component={HomeComponent} />
             <Route path="/randomdots" exact component={RandomDotsContainer} />
+            <Route path="/rain" exact component={RainContainer} />
           </Switch>
 
         </div>
---
+++ client/src/components_container/rainContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class RainContainer extends Component {
+
+  render() {
+    return (
+      <p> You made it to the rain container! </p>
+    )
+  }
+}
+
+export default RainContainer
