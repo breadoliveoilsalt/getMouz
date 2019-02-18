33877906fec12e66ac624243c4ec8d03571d2598
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-02-18 15:17:41 -0500

    In middle of getting RainDrop component to render. Added notes for next steps...
    ...to rain container component."

--- client/src/components_container/rainContainer.js
+++ client/src/components_container/rainContainer.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import '../parentCSS.css'
 import { createRainFactory, addRainDropFactory, addRainDrop, clearRainDropFactoryAndRainDrops } from '../actions/rainActions'
 
+import RainDrop from './rainDropComponent'
+
 class RainContainer extends Component {
 
   componentDidMount() {
@@ -10,7 +12,7 @@ class RainContainer extends Component {
     this.props.addRainDropFactory(rdFactory)
     // let rainDrop = this.props.rainDropFactory.createRainDrop()
     // this.props.addRainDrop(rainDrop)
-    this.timer = setInterval(() => this.renderRainDrop(), 750)
+    this.timer = setInterval(() => this.renderRainDrop(), 2000)
 
   }
 
@@ -21,40 +23,49 @@ class RainContainer extends Component {
 
   render() {
 
-    const segmentStyle1 = {
-      position: "absolute",
-      backgroundColor: "white",
-      height: "1em",
-      width: ".2em",
-      left: "10em",
-      bottom: "29em"
-    }
+    // const segmentStyle1 = {
+    //   position: "absolute",
+    //   backgroundColor: "white",
+    //   height: "1em",
+    //   width: ".2em",
+    //   left: "10em",
+    //   bottom: "29em"
+    // }
+    //
+    // const segmentStyle2 = {
+    //   position: "absolute",
+    //   backgroundColor: "white",
+    //   height: "1em",
+    //   width: ".2em",
+    //   left: "10.1em",
+    //   bottom: "11em"
+    // }
+    //
+    // const segmentStyle3 = {
+    //   position: "absolute",
+    //   backgroundColor: "white",
+    //   height: "1em",
+    //   width: ".2em",
+    //   left: "10.2em",
+    //   bottom: "12em"
+    // }
 
-    const segmentStyle2 = {
-      position: "absolute",
-      backgroundColor: "white",
-      height: "1em",
-      width: ".2em",
-      left: "10.1em",
-      bottom: "11em"
-    }
+    // const dropSegments = [<div style={segmentStyle1} />,  <div style={segmentStyle2} />, <div style={segmentStyle3} />]
 
-    const segmentStyle3 = {
-      position: "absolute",
-      backgroundColor: "white",
-      height: "1em",
-      width: ".2em",
-      left: "10.2em",
-      bottom: "12em"
-    }
 
-    const dropSegments = [<div style={segmentStyle1} />,  <div style={segmentStyle2} />, <div style={segmentStyle3} />]
+    // 190218 - up to: go to reducer and make raindrops one big object where each key is the id
+    // and the values are the segments. Then see if I can get these things to render on page or in display.
+    // Fixed the above - see if I can add key to dots and rainDrops.
+    // Don't forget to add componentWillUnmount here!
+    const drops = this.props.rainDrops.map( (rainObject) => {
+      return <RainDrop objectInfo={rainObject} />
+    })
 
     return (
       <div>
         <h1 className="text-centered">Rain</h1>
         <div className="container">
-          {dropSegments}
+          {drops}
         </div>
       </div>
     )
---
+++ client/src/components_container/rainDropComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class RainDrop extends Component {
+
+  render() {
+    return (
+      <div> You've got a drop </div>
+    )
+  }
+}
+
+
+
+export default RainDrop
