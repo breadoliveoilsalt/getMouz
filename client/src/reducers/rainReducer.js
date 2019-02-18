08dad1501da4fa12926418752f4c41986fcdded1
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-02-17 19:54:11 -0500

    Draft rainReducer and incorporate into rootReducer

---
+++ client/src/reducers/rainReducer.js
@@ -0,0 +1,18 @@
+function rainReducer(state = {
+  rainDropFactory: null,
+  rainDrops: []
+  },
+  action) {
+    switch(action.type) {
+      case 'ADD_RAIN_DROP_FACTORY':
+        return Object.assign({}, state, {rainDropFactory: action.payload})
+      case 'ADD_DROP':
+        return Object.assign({}, state, {rainDrops: [...state.rainDrops, ...action.payload]})
+      case 'CLEAR_RAIN_DROP_FACTORY_AND_RAIN_DROPS':
+        return {dotFactory: null, dots: []}
+      default:
+        return state
+      }
+}
+
+export default rainReducer
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -7,11 +7,13 @@ import { combineReducers } from 'redux'
 
 // import testReducer from './testReducer'
 import randomDotsReducer from './randomDotsReducer'
+import rainReducer from './rainReducer'
 
 const rootReducer = combineReducers(
   {
     // testReducer: testReducer,
-    randomDots: randomDotsReducer
+    randomDots: randomDotsReducer,
+    rain: rainReducer
   }
 )
 
