df45f02c18e5eee3b67255e9a3598f9ec218f250
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-02-27 17:11:12 -0500

    Delete comments from rain reducer on what did not work

--- client/src/reducers/rainReducer.js
+++ client/src/reducers/rainReducer.js
@@ -14,13 +14,6 @@ function rainReducer(state = {
         let stateCopy = Object.assign({}, state)
         delete stateCopy.rainDrops[action.payload]
         return stateCopy
-        // This makes separate copies of the rainDrop to be deleted and the rest of the rainDrops...
-        // 190224 up to here.
-        //let idToDelete = action.payload
-        // let { idToDelete, ...restOfRainDrops } = state.rainDrops
-
-        // ...and then adds back to the rainDrop state the rest of the rainDrops.
-        // return Object.assign({}, state, {rainDrops: {restOfRainDrops} })
       case 'CLEAR_RAIN_DROP_FACTORY_AND_RAIN_DROPS':
         return {rainDropFactory: null, rainDrops: []}
       default:
@@ -29,14 +22,3 @@ function rainReducer(state = {
 }
 
 export default rainReducer
-
-// Note for rainDrops above, I originally thought this...
-// raindrops: {
-// id: null
-// segments: []
-// }
-// ...but then I thought this would be easier to look up and iterate:
-// raindrops: {id: [segments], id: [segments], etc.}
-// ...but problem is, I cannot use numbers for keys unless I use a JS map.  That's getting a big complicated.
-// Seems there are issues with this: https://github.com/reduxjs/redux/issues/1499
-// ...so going to make raindrops a 2D array, ignoring ids, for now.
