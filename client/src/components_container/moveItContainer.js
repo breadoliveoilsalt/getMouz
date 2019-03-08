13977022da5aa8fc69c389ef2c9b18fc71814049
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-03-08 16:59:22 -0500

    To MoveItContainer, add cat image header and center cat image in container.

--- client/src/components_container/moveItContainer.js
+++ client/src/components_container/moveItContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createRainFactory, addRainDropFactory, addRainDrop, updateRainDrop, clearRainDrop, clearRainDropFactoryAndRainDrops } from '../actions/rainActions'
+import catImage from '../images/cat-small.png'
+
 
 
 class MoveItContainer extends Component {
@@ -8,9 +10,10 @@ class MoveItContainer extends Component {
   render() {
     return (
       <div>
-        <h1 className="text-centered">Make the Cat Move It</h1>
+        <h1 className="text-centered">Make the Cat Move It   </h1>
+        <img id={"cat-header-image"} className="text-centered" src={catImage} />
         <div className="container">
-
+          <img id={"cat-game-image"} src={catImage} style={{display: "inline-block", position: "absolute", bottom: "14em", left: "14em"}}/>
         </div>
       </div>
     )
