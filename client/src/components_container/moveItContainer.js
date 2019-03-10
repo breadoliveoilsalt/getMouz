9998f8baa970debde386bf948c9a6ad79d476ad0
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-03-10 12:20:34 -0400

    In moveItContainer, cat position is now determined by redux state and has separate style const variable

--- client/src/components_container/moveItContainer.js
+++ client/src/components_container/moveItContainer.js
@@ -10,14 +10,22 @@ class MoveItContainer extends Component {
     console.log("you key'd down")
     console.log("here's your event", e.which)
   }
+// up: 38; down: 40; left: 37; right: 39
+
 
   render() {
+
+    const catPositionStyle = {
+      bottom: `${this.props.catPosition.bottom}em`,
+      left: `${this.props.catPosition.left}em`
+    }
+
     return (
       <div>
         <h1 className="text-centered">Make the Cat Move It   </h1>
         <img id={"cat-header-image"} className="text-centered" src={catImage} />
         <div className="container" onKeyDown={this.moveCat} tabIndex="0">
-          <img id={"cat-game-image"} src={catImage} style={{}}/>
+          <img id={"cat-game-image"} src={catImage} style={catPositionStyle}/>
         </div>
       </div>
     )
