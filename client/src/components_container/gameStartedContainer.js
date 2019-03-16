bed366d733465bf6e47dc2182d1c9574f223c296
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-03-16 18:54:22 -0400

    Move the cat movement lfunctions so far to the GameStartedContainer

--- client/src/components_container/gameStartedContainer.js
+++ client/src/components_container/gameStartedContainer.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import { updateCatPosition } from '../actions/moveItActions'
+import catImage from '../images/cat-small.png'
+
 class GameStartedContainer extends Component {
 
 
   componentDidMount() {
+      // need 'ref' in container div below so focus is on div when component loads and keys will trigger movement
     this._gameContainer.focus()
   }
 
+  moveCat = (e) => {
+    e.preventDefault()
+    console.log("key", e.keyCode)
+
+    // moving up:
+    if (e.keyCode === 38 && this.props.catPosition.bottom < 27) {
+      this.props.updateCatPosition({
+        left: this.props.catPosition.left,
+        bottom: this.props.catPosition.bottom + 1
+      })
+    }
+    // moving down:
+    if (e.keyCode === 40 && this.props.catPosition.bottom > 0) {
+      this.props.updateCatPosition({
+        left: this.props.catPosition.left,
+        bottom: this.props.catPosition.bottom - 1
+      })
+    }
+    // moving left:
+    if (e.keyCode === 37 && this.props.catPosition.left > 0) {
+      this.props.updateCatPosition({
+        left: this.props.catPosition.left - 1,
+        bottom: this.props.catPosition.bottom
+      })
+    }
+    // moving right:
+    if (e.keyCode === 39 && this.props.catPosition.left < 27) {
+      this.props.updateCatPosition({
+        left: this.props.catPosition.left + 1,
+        bottom: this.props.catPosition.bottom
+      })
+    }
+  }
+
+
   render() {
 
+    // put if mouse logic here
     const catPositionStyle = {
       bottom: `${this.props.catPosition.bottom}em`,
       left: `${this.props.catPosition.left}em`,
@@ -18,31 +58,29 @@ class GameStartedContainer extends Component {
     }
 
     return (
-      <div ref={d => (this._gameContainer = d)} className="container" onKeyDown={this.props.moveCat} tabIndex="0">
-        <img id={"cat-game-image"} src={this.props.catImage} style={catPositionStyle}/>
+      <div ref={d => (this._gameContainer = d)} className="container" onKeyDown={this.moveCat} tabIndex="0">
+        <img id={"cat-game-image"} src={catImage} style={catPositionStyle}/>
       </div>
     )
 
   }
 }
 
-export default GameStartedContainer
+const mapStateToProps = (state) => {
+  return {
+    catPosition: state.moveIt.catPosition,
+    // gameStarted: state.moveIt.gameStarted
+  }
+}
 
-// const mapStateToProps = (state) => {
-//   return {
-//     catPosition: state.moveIt.catPosition,
-//     gameStarted: state.moveIt.gameStarted
-//   }
-// }
-//
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     updateCatPosition: (coordinates) => dispatch(updateCatPosition(coordinates)),
-//     startGame: () => dispatch(startGame())
-//    }
-// }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateCatPosition: (coordinates) => dispatch(updateCatPosition(coordinates)),
+    // startGame: () => dispatch(startGame())
+   }
+}
 
-// export default connect(mapStateToProps, mapDispatchToProps)(MoveItContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(GameStartedContainer)
 
 
 // export default MoveItContainer
--- client/src/components_container/moveItContainer.js
+++ client/src/components_container/moveItContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { startGame, updateCatPosition } from '../actions/moveItActions'
+import { startGame } from '../actions/moveItActions'
 import GameNotStartedContainer from './gameNotStartedContainer'
 import GameStartedContainer from './gameStartedContainer'
 import catImage from '../images/cat-small.png'
@@ -10,41 +10,7 @@ import catImage from '../images/cat-small.png'
 class MoveItContainer extends Component {
 
   componentWillUnmount() {
-    // up to add action creator to restart game when another link is clicked 
-  }
-
-  moveCat = (e) => {
-    e.preventDefault()
-    console.log("key", e.keyCode)
-
-    // moving up:
-    if (e.keyCode === 38 && this.props.catPosition.bottom < 27) {
-      this.props.updateCatPosition({
-        left: this.props.catPosition.left,
-        bottom: this.props.catPosition.bottom + 1
-      })
-    }
-    // moving down:
-    if (e.keyCode === 40 && this.props.catPosition.bottom > 0) {
-      this.props.updateCatPosition({
-        left: this.props.catPosition.left,
-        bottom: this.props.catPosition.bottom - 1
-      })
-    }
-    // moving left:
-    if (e.keyCode === 37 && this.props.catPosition.left > 0) {
-      this.props.updateCatPosition({
-        left: this.props.catPosition.left - 1,
-        bottom: this.props.catPosition.bottom
-      })
-    }
-    // moving right:
-    if (e.keyCode === 39 && this.props.catPosition.left < 27) {
-      this.props.updateCatPosition({
-        left: this.props.catPosition.left + 1,
-        bottom: this.props.catPosition.bottom
-      })
-    }
+    // up to add action creator to restart game when another link is clicked
   }
 
   render() {
@@ -54,7 +20,7 @@ class MoveItContainer extends Component {
       return (
         <div>
           <h1 className="text-centered">Make the Cat Move It</h1>
-          <GameStartedContainer moveCat={this.moveCat} catImage={catImage} catPosition={this.props.catPosition}/>
+          <GameStartedContainer />
         </div>
       )
     } else {
@@ -78,7 +44,6 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateCatPosition: (coordinates) => dispatch(updateCatPosition(coordinates)),
     startGame: () => dispatch(startGame())
     // up to add restart game
    }
