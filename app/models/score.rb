f7f5116c214a14326597f9b9160cb209568162b3
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-04-15 20:23:06 -0400

    In score model for Rails, add get_top_five and delete_excess_scores methods

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,20 @@
 class Score < ApplicationRecord
   validates :points, numericality: { only_integer: true }
-  
+
+  def self.get_top_five
+    self.order(points: :desc).limit(5)
+  end
+
+  def self.delete_excess_scores
+    if self.count > 10
+      ordered_scores = self.order(points: :desc)
+      print "Ordered Scores: #{ordered_scores}"
+      (10...ordered_scores.length).each do |index|
+        print "Current recored: #{ordered_scores[index]}"
+        ordered_scores[index].delete
+      end
+    end
+  end
+
+
 end
