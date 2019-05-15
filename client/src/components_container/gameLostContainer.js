83550036ac2c110ad1a820b0ac59a60b54220a3f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-05-15 11:05:45 -0400

    Refactoring - clean up to gameLostContainer

--- client/src/components_container/gameLostContainer.js
+++ client/src/components_container/gameLostContainer.js
@@ -29,22 +29,3 @@ class GameLostContainer extends Component {
 }
 
 export default GameLostContainer
-
-// import React from 'react'
-// import gameLostImage from '../images/game-lost-image.png'
-//
-// const GameLostContainer = (props) => {
-//
-//   return(
-//
-//       <div className="game-screen" tabIndex="0">
-//
-//           <a onClick={props.restartGame}>
-//             <img className="cat-menu-image" src={gameLostImage} />
-//           </a>
-//         </div>
-//     )
-//
-// }
-//
-// export default GameLostContainer
