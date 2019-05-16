bbd27e24f8ae0e60f6b3adee5071bdf27a7b22e1
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-05-16 09:22:07 -0400

    Refactoring - Clean up to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get "/api/getMessage", to: "test#test_message"
   get "/api/scores", to: "scores#index"
   post "/api/scores", to: "scores#create"
 
